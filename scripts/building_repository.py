182402907f52e37dbdbefefea1fa0cf6bba393bc
Ankitkumar Jain <aajain2@ncsu.edu>
2018-04-01 18:45:31 -0400

    Adding functionality to build the tweets repository in the cassandra database

--- scripts/building_repository.py
+++ scripts/building_repository.py
@@ -7,19 +7,52 @@ import jsonpickle
 import os
 import sys
 from zipfile import ZipFile
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement
+from cassandra.query import SimpleStatement
+import re
+import datetime
+from cassandra.util import uuid_from_time, datetime_from_uuid1
 
 
-def main():
+def unzipTweets():
     """Unzip the compressed tweets"""
-    #directory = os.fsencode("../data")
     root="../data/"
     for file in os.listdir(root):
         filename = os.fsdecode(file)
         if filename.endswith(".zip"):
-            print(filename)
+            print("unzipping ",filename)
             with ZipFile(root+filename,"r") as zip_ref:
                 zip_ref.extractall("../data")
     
+def buildDB():
+    """Build the database"""
+    #create instance of local cassandra cluster
+    cluster = Cluster()
+    #connect to 'tweet_mining' keyspace
+    session = cluster.connect('tweet_mining')
+    tweet_insert = session.prepare("INSERT INTO tweets (tweet_id, tweet_text, favorite_count, retweet_count) VALUES (?, ?, ?, ?)")
+    root="../data/data/"
+    #Using a translation table to map everything outside of the BMP (emojis) to the replacement character
+    non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+    for file in os.listdir(root):
+        batch = BatchStatement()
+        with open(root+file,"r") as fileHandle:
+            #batch.add(tweet_insert, (name, age))
+            filedata =json.load(fileHandle)
+            for data in filedata:
+                try:
+                    tweet_text = str(data.get('text')).translate(non_bmp_map)
+                    if data.get('id_str'):
+                        batch.add(tweet_insert,(data.get('id_str'),tweet_text,data.get('favorite_count'),data.get('retweet_count')))
+                except:
+                    print (sys.exc_info())
+            session.execute(batch)
+            
+
+def main():
+    unzipTweets()
+    buildDB()
 
 
 if __name__ == '__main__':
