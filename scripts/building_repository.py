d7c401baa76af6947e729fafe49d0734f9732505
Ankitkumar Jain <aajain2@ncsu.edu>
2018-04-01 21:53:18 -0400

    Storing language in the database

--- scripts/building_repository.py
+++ scripts/building_repository.py
@@ -39,7 +39,7 @@ def buildDB():
     #connect to 'tweet_mining' keyspace
     session = cluster.connect('tweet_mining')
     #creating dynamic CQL query
-    tweet_insert = session.prepare("INSERT INTO tweets (tweet_id, tweet_text, favorite_count, retweet_count) VALUES (?, ?, ?, ?)")
+    tweet_insert = session.prepare("INSERT INTO tweets (tweet_id, tweet_text, favorite_count, retweet_count,lang) VALUES (?, ?, ?, ?, ?)")
     root="../data/data/"
     #Using a translation table to map everything outside of the BMP (emojis) to the replacement character
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
@@ -53,7 +53,7 @@ def buildDB():
                     #filtering non-bmp characters from tweet text
                     tweet_text = str(data.get('text')).translate(non_bmp_map)
                     if data.get('id_str'):
-                        batch.add(tweet_insert,(data.get('id_str'),tweet_text,data.get('favorite_count'),data.get('retweet_count')))
+                        batch.add(tweet_insert,(data.get('id_str'),tweet_text,data.get('favorite_count'),data.get('retweet_count'),data.get('lang')))
                 except:
                     print (sys.exc_info())
             print("Processing Batch of file : ",file)
