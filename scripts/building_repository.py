761e0a708626fed6daccb0d460e53f9a93c56a38
Chirag Jain <csjain@ncsu.edu>
2018-04-03 18:11:45 -0400

    PEP8 guideline code updated

--- scripts/building_repository.py
+++ scripts/building_repository.py
@@ -34,32 +34,49 @@ def unzipTweets():
 def buildDB():
     """Build the database."""
     # Create instance of local cassandra cluster
+
     cluster = Cluster()
+
     # Connect to 'tweet_mining' keyspace
     session = cluster.connect('tweet_mining')
+
     # Creating dynamic CQL query
-    tweet_insert = session.prepare("INSERT INTO tweets (tweet_id, tweet_text, favorite_count, retweet_count,lang) VALUES (?, ?, ?, ?, ?)")
+    qryText = """INSERT INTO tweets (tweet_id, tweet_text, search_query,
+                                    favorite_count, retweet_count, lang,
+                                    tweet_polarity, tweet_subjectivity)
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+
+    tweet_insert = session.prepare(qryText)
     root = '../data/data/'
-    #Using a translation table to map everything outside of the BMP (emojis) to the replacement character
+
+    # Using a translation table to map everything outside of the BMP (emojis)
+    # to the replacement character
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
     for file in os.listdir(root):
         batch = BatchStatement()
-        with open(root+file,"r") as fileHandle:
-            #batch.add(tweet_insert, (name, age))
-            filedata =json.load(fileHandle)
+        with open(root+file, 'r') as fileHandle:
+            # batch.add(tweet_insert, (name, age))
+            filedata = json.load(fileHandle)
             for data in filedata:
                 try:
-                    #filtering non-bmp characters from tweet text
+                    # Filtering non-bmp characters from tweet text
                     tweet_text = str(data.get('text')).translate(non_bmp_map)
                     if data.get('id_str'):
-                        batch.add(tweet_insert,(data.get('id_str'),tweet_text,data.get('favorite_count'),data.get('retweet_count'),data.get('lang')))
-                except:
-                    print (sys.exc_info())
-            print("Processing Batch of file : ",file)
+                        batch.add(tweet_insert, (data.get('id_str'),
+                                                 tweet_text,
+                                                 data.get('favorite_count'),
+                                                 data.get('retweet_count'),
+                                                 data.get('lang')))
+                except Exception as exp:
+                    print('Error: {}'.format(exp))
+                    print(sys.exc_info())
+            print('Processing Batch of file : {}'.format(file))
             session.execute(batch)
     shutil.rmtree('../data/data')
 
+
 def main():
+    """Initialize everything."""
     unzipTweets()
     if not os.path.exists('../data/data'):
         print("There are no new tweets to add to the database")
