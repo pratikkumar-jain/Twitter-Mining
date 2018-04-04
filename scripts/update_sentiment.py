c6d84d2b491cddab622a6b4e26c1303780046c15
Chirag Jain <csjain@ncsu.edu>
2018-04-04 11:05:23 -0400

    Get tweet function coded

--- scripts/update_sentiment.py
+++ scripts/update_sentiment.py
@@ -3,20 +3,32 @@
 """Script to update the sentiment values for tweets."""
 """which have not been analyzed or are newly added to database."""
 
+from cassandra.cluster import Cluster
+from textblob import TextBlob
 
-def getTweets(conn):
+
+def getTweets(session):
     """Get IDs and text for which analysis is pending."""
     tweets = {}
+
+    # Creating dynamic CQL query
+    qryText = None
+    with open('get_tweets_query.sql', 'r') as qryHandle:
+        qryText = qryHandle.read().strip()
+
+    results = session.execute(qryText)
+
+    for result in results:
+        tweets[result.tweet_id] = result.tweet_text
 
     return tweets
 
 
 def performSentimentAnalysis(tweetText):
     """Analyze sentiment."""
-    subjectivity = 0
-    polarity = 0
+    blob = TextBlob(tweetText)
 
-    return subjectivity, polarity
+    return blob.sentiment.subjectivity, blob.sentiment.polarity
 
 
 def updateSentiment(tweetId, subjectivity, polarity):
@@ -25,6 +37,15 @@ def updateSentiment(tweetId, subjectivity, polarity):
 
 def main():
     """Initialize everything."""
+    # Create instance of local cassandra cluster
+    cluster = Cluster()
+
+    # Connect to 'tweet_mining' keyspace
+    session = cluster.connect('tweet_mining')
+
+
+
+
 
 
 if __name__ == '__main__':
