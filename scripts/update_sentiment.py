19ec777acacd2a87722cde8a03fb7ce8e513707f
Chirag Jain <csjain@ncsu.edu>
2018-04-04 10:50:11 -0400

    defined code structure

--- scripts/update_sentiment.py
+++ scripts/update_sentiment.py
@@ -4,6 +4,25 @@
 """which have not been analyzed or are newly added to database."""
 
 
+def getTweets(conn):
+    """Get IDs and text for which analysis is pending."""
+    tweets = {}
+
+    return tweets
+
+
+def performSentimentAnalysis(tweetText):
+    """Analyze sentiment."""
+    subjectivity = 0
+    polarity = 0
+
+    return subjectivity, polarity
+
+
+def updateSentiment(tweetId, subjectivity, polarity):
+    """Update tweet with sentiment."""
+
+
 def main():
     """Initialize everything."""
 
