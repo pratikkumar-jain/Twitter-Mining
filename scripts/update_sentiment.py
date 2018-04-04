895fe093d27d424ccd67781b873b0d5b0db1a505
Chirag Jain <csjain@ncsu.edu>
2018-04-04 10:32:28 -0400

    File creation

---
+++ scripts/update_sentiment.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+"""Script to update the sentiment values for tweets."""
+"""which have not been analyzed or are newly added to database."""
+
+
+def main():
+    """Initialize everything."""
+
+
+if __name__ == '__main__':
+    main()
