79cd5da733a2053f2276665853a99ba64b3fd2cf
Pratik Jain <pjain22@ncsu.edu>
2018-04-05 15:36:30 -0400

    Adding Wordnet Lemmatizer

--- scripts/yelp_review_mining.py
+++ scripts/yelp_review_mining.py
@@ -1,15 +1,19 @@
-import json
 import enchant
+import json
+
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
-# from nltk.corpus import stopwords
 
 # File Path : Change accordingly
 filename = "/Users/pratik/Downloads/dataset/review.json"
 output_text = []
 
+lemmatizer = WordNetLemmatizer()
 
 # function to read a review and append it to the list
 # if it is useful
+
 def filereader(line):
     gdfJson = json.loads(line)
     if gdfJson['useful'] > 0:
@@ -34,6 +38,7 @@ for reviews in output_text:
     filtered_words = []
     for word in reviews:
         if d.check(word):
+            lemmatizer.lemmatize(word)
             filtered_words.append(word)
     output_text2.append(''.join(filtered_words))
 
