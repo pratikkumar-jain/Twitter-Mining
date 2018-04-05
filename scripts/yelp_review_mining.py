5f15eb85117dd62cf1cddf64637febb2a30fd640
Pratik Jain <pjain22@ncsu.edu>
2018-04-05 15:57:01 -0400

    Adding RegexpTokenizer to remove punctuations

--- scripts/yelp_review_mining.py
+++ scripts/yelp_review_mining.py
@@ -3,6 +3,7 @@ import json
 
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 # File Path : Change accordingly
@@ -14,10 +15,11 @@ lemmatizer = WordNetLemmatizer()
 # function to read a review and append it to the list
 # if it is useful
 
+
 def filereader(line):
-    gdfJson = json.loads(line)
-    if gdfJson['useful'] > 0:
-        out = gdfJson['text'].replace('\n', '')
+    review_line = json.loads(line)
+    if review_line['useful'] > 0:
+        out = review_line['text'].replace('\n', '')
         output_text.append(out)
 
 
@@ -30,17 +32,19 @@ with open(filename, 'r') as f:
         else:
             break
 
+tokenizer = RegexpTokenizer(r'\w+')
+
 # package to check if a word is in Dictionary
 d = enchant.Dict("en_US")
 
 output_text2 = []
 for reviews in output_text:
     filtered_words = []
-    for word in reviews:
+    for word in tokenizer.tokenize(reviews):
         if d.check(word):
             lemmatizer.lemmatize(word)
             filtered_words.append(word)
-    output_text2.append(''.join(filtered_words))
+    output_text2.append(' '.join(filtered_words))
 
 # function to create Bag of Words by removing stop words
 vectorizer = CountVectorizer(stop_words='english')
