c0b699d0ff338bb532178681c7e9e9aa2eefc519
Ankitkumar Jain <aajain2@ncsu.edu>
2018-04-05 17:28:51 -0400

    Changing the extension

--- scripts/insert_query.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tweets (tweet_id, tweet_text, search_query,
-                                  favorite_count, retweet_count, lang,
-                                  tweet_polarity, tweet_subjectivity)
-                                  VALUES (?, ?, ?, ?, ?, ?, -10, -10)
