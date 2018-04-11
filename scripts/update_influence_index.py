f9c631cd58efbbbfbec26c22227773d4f7d3c0c6
rishabh05apr <rishabh05apr@gmail.com>
2018-04-11 14:30:04 -0400

    Influence_Index

--- scripts/update_influence_index.py
+++ scripts/update_influence_index.py
@@ -47,3 +47,57 @@ def batchUpdate(batch, session, startId, endId, counter):
         print('Update Success')
     except Exception as exp:
         print('Exception in updating: {}, Update Failed'.format(exp))
+
+
+
+
+def main():
+    """Initialize everything."""
+
+    # Create instance of local cassandra cluster
+    cluster = Cluster()
+
+    # Connect to 'tweet_mining' keyspace
+    session = cluster.connect('tweet_mining')
+
+    tweets = getTweets(session)
+
+    with open('update_influence_index_query.cql', 'r') as qryHandle:
+        qryText = qryHandle.read().strip()
+
+    tweetUpdate = session.prepare(qryText)
+
+    counter = 0
+    processed = 0
+    batchSize = 500
+
+    # Counter for normalizing tweets
+    max_influence_result = session.execute('select max(user_followers_count) as max_influence from tweets;')
+    max_influence = max_influence_result.current_rows[0].max_influence
+    min_influence_result = session.execute('select min(user_followers_count) as min_influence from tweets;')
+    min_influence = min_influence_result.current_rows[0].min_influence
+
+    print (max_influence)
+    print (min_influence)
+
+    tweetUpdate = session.prepare(qryText)
+
+    for tweetId, tweetObj in tweets.items():
+
+        if counter == 0:
+            startId = tweetId
+            batch = BatchStatement()
+
+        normalized_ii_value = calculateNormalizedInfluenceIndex(max_influence, min_influence, tweetObj)
+
+        batch.add(tweetUpdate, (normalized_ii_value, tweetId))
+        processed += 1
+        counter += 1
+
+        if counter % batchSize == 0 or processed == len(tweets):
+            endId = tweetId
+            batchUpdate(batch, session, startId, endId, counter)
+            counter = 0
+
+if __name__ == '__main__':
+    main()
